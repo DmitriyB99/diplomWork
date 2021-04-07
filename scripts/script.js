dc6627582f2d2b64837fabe7f5a20c25fa20fa91
Dmitriy <dmitry9989@gmail.com>
2021-04-08 01:58:46 +0600

    second

--- scripts/script.js
+++ scripts/script.js
@@ -8,10 +8,42 @@ const mySwiper = new Swiper('.swiper-container', {
 
 const menuSwiper = new Swiper('.swiper-container-menu', {
     slidesPerView: 'auto',
-    spaceBetween: 95,
-    
+    spaceBetween: 63,    
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     }
 });
+
+$('.rev-swiper').on('shown.bs.tab', function(e) {
+    const swiperReviews = new Swiper('.slider-reviews', {
+        slidesPerView: 2,
+        spaceBetween: 16,
+        
+        navigation: {
+            nextEl: '.swiper-button-next',
+        },
+    });
+})
+
+
+
+jQuery(function(){
+    var j = jQuery;
+    var addInput = '#qty';
+    var n = 1;
+
+    j(addInput).val(n);
+
+    j('.plus').on('click', function(){
+        j(addInput).val(++n);
+    })
+
+    j('.min').on('click', function(){
+        if (n >= 1) {
+            j(addInput).val(--n);
+        } else{
+            
+        }
+    })
+})
